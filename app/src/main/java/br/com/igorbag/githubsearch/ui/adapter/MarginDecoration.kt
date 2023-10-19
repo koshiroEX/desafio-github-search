d460aa4bf1b19979433369598c13c1a4b5c9e441
Marcio "Koshiro" Menezes <118092843+koshiroEX@users.noreply.github.com>
2023-10-19 10:47:14 -0300

    implementação da funcionalidade, cumprindo desafio

---
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/MarginDecoration.kt
@@ -0,0 +1,17 @@
+package br.com.igorbag.githubsearch.ui
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MarginDecoration(private val spaceSize: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect, view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        with(outRect) {
+            bottom = spaceSize
+        }
+    }
+}
